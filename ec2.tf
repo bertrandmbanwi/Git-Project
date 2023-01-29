65257cfe79a52e7d48ced74cddd144f8759b768b
Beco <bertrandmbanwi@gmail.com>
2023-01-29 11:21:34 -0500

    modified as per request

--- ec2.tf
+++ ec2.tf
@@ -7,3 +7,29 @@ resource "aws_instance" "web" {
     }
   
 }
+
+# Path: ec2.tf
+resource "aws_security_group" "web" {
+    name        = "terraform"
+    description = "Allow SSH and HTTP traffic"
+    ingress {
+        description = "SSH"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["10.0.1.0/24"]
+    }
+    ingress {
+        description = "HTTP"
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["10.0.1.0/24"]
+    }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["10.0.1.0/24"]
+    }   
+}
