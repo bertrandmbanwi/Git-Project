12330af74236b5860dce7d713978c6c1daaf90d7
Beco <bertrandmbanwi@gmail.com>
2023-02-01 12:27:43 -0500

    Java Spring application with an API endpoint that converts an integer into a Roman numeral

--- Work-to-do/roman-numeral-converter.java
+++ Work-to-do/roman-numeral-converter.java
@@ -1,85 +1,35 @@
 @RestController
-public class RomanNumeralController {
+public class RomanNumeralConverterController {
 
-    // Endpoint to convert integer to roman numeral
-    @GetMapping("/convert")
-    public String convert(@RequestParam int value) {
-        // Logic to convert integer to roman numeral
+    @GetMapping("/convert/{number}")
+    public String convert(@PathVariable int number) {
+        return RomanNumeralUtil.convert(number);
+    }
+
+}
+
+public class RomanNumeralUtil {
+    public static String convert(int num) {
+        StringBuilder sb = new StringBuilder();
+
+        // List of Roman Numerals
+        String[] romanNumerals = {"M", "CM", "D", "CD",
+            "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        // List of Decimals
+        int[] decimals = {1000, 900, 500, 400, 100, 90, 50,
+            40, 10, 9, 5, 4, 1};
+
+        // Converting to Roman
+        for (int i = 0; i < decimals.length; i++) {
+            while (num % decimals[i] < num) {
+                sb.append(romanNumerals[i]);
+                num -= decimals[i];
+            }
+        }
+        return sb.toString();
     }
 }
 
-//The logic to convert an integer to a roman numeral can be implemented using a switch statement, 
-//or by using a Map to look up the corresponding roman numeral for each integer.
-switch (value) {
-    case 1:
-        return "I";
-    case 5:
-        return "V";
-    case 10:
-        return "X";
-    case 50:
-        return "L";
-    case 100:
-        return "C";
-    case 500:
-        return "D";
-    case 1000:
-        return "M";
-    default:
-        return "";
-}
-
-//Here is an example of a Map implementation:
-
-Map<Integer, String> map = new HashMap<>();
-map.put(1, "I");
-map.put(5, "V");
-map.put(10, "X");
-map.put(50, "L");
-map.put(100, "C");
-map.put(500, "D");
-map.put(1000, "M");
-
-String romanNumeral = map.get(value);
-
-//Once the logic is implemented, you can add the endpoint to your controller in 
-//order to handle the conversion:
-
-
-
-//To create a CI/CD configuration using AWS CodePipeline, you will need to create a pipeline 
-//and add the stages that are needed to deploy your application.
-
-// Create a CodePipeline client
-AmazonCodePipeline client = AmazonCodePipelineClientBuilder.standard().build();
-
-// Create a pipeline
-CreatePipelineRequest createPipelineRequest = new CreatePipelineRequest()
-    .withName("MyPipeline")
-    .withRoleArn("arn:aws:iam::<account_id>:role/<role_name>")
-    .withArtifactStore(new ArtifactStore()
-        .withType("S3")
-        .withLocation("<bucket_name>"));
-
-client.createPipeline(createPipelineRequest);
-
-// Add stages to the pipeline
-PutPipelineDefinitionRequest putPipelineDefinitionRequest = new PutPipelineDefinitionRequest()
-    .withPipelineName("MyPipeline")
-    .withPipelineDefinition(new PipelineDefinition()
-        .withStages(new StageDeclaration()
-            .withName("Source")
-            .withActions(new ActionDeclaration()
-                .withName("Source")
-                .withActionTypeId(new ActionTypeId()
-                    .withCategory("Source")
-                    .withOwner("ThirdParty")
-                    .withProvider("GitHub")
-                    .withVersion("1"))
-                .withConfiguration(new Configuration()
-                    .withSecretArn("arn:aws:secretsmanager:<region>:<account_id>:secret:
-                        <secret_name>-<secret_id>") 
-                    .withOwner("aws-samples")
-                    
 
 
