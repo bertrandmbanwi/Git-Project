fda1d1c70803f651ea03930011a7daab0a3b98d5
Beco <bertrandmbanwi@gmail.com>
2023-02-01 20:35:11 -0500

    Create a Java Spring application with an API endpoint that converts an integer into a roman numeral.

--- Work-to-do/RomanNumeralConverterApplication.java
+++ Work-to-do/RomanNumeralConverterApplication.java
@@ -73,3 +73,18 @@ public class RomanNumeralConverterApplication {
         }
     }
 }
+
+
+//This code creates a Spring Boot application that provides a single endpoint, /convert/{number}, 
+//which takes an integer as input and returns the equivalent Roman numeral as a string. 
+//The input number is passed as a path variable in the URL and is accessible in the method using 
+//the @PathVariable annotation. 
+//The conversion process is implemented using a loop that decrements the input integer by the 
+//value of the largest possible Roman numeral symbol, adds the corresponding symbol to a result string, 
+//and repeats the process until the input integer is zero. 
+//The code also checks if the input number is within the valid range of 1 to 3999 before performing the conversion.
+
+//To run the code, you can simply create a new project in your Java development environment, 
+//copy and paste the code into a file named RomanNumeralConverterApplication.java, and run the main method. 
+//The application will start and you can access the API endpoint at http://localhost:8080/convert/{number}, 
+//where {number} should be replaced with an integer between 1 and 3999
